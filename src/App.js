dea0a53b85a01cd651c8bb94091551644f5f77a0
Arun <arunkumard0606@gmail.com>
2025-01-25 17:48:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world!</p>
+      <img src={require('./images/Arun.jpeg')}
+       className="pic" alt="Profile"/>
+    </div>
+  );
+}
+
+export default App;
