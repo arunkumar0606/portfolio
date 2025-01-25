ba6da441dccf46e79564bd68ad087b7e6c9fac49
Arun <arunkumard0606@gmail.com>
2025-01-25 18:57:42 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ function App() {
     <div className="App">
       <p>Welcome to my Profile!</p>
       <img src={require('./images/Arun.jpeg')}
-       className="pic" alt="Profile"/>
+        className="pic" alt="Profile" />
+      <p>Hi i'm Arun. Currently working as a software developer with over 2.5+yrs of experience.
+        Proficient in JAVA development. I love to learn new everyday.
+      </p>
+      <p>Follow me:       <a href="https://www.linkedin.com/in/arunkumard0606/" > Linkdin</a> </p>
+
+      <footer class="footer">
+        <div class="footer-container">
+          <p> Made with ❤ by Arun</p>
+        </div>
+      </footer>
     </div>
+
   );
 }
 
