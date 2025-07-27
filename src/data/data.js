db1ba51f136ba716fd8982a06f49f0a0ce83bd0b
Arun <arunkumard0606@gmail.com>
2025-07-27 20:39:07 +0530

    updated data

--- src/data/data.js
+++ src/data/data.js
@@ -9,14 +9,14 @@ export const timelineItems = [
     {
       title: "Systems Engineer",
       subtitle: "TCS",
-      date: "2021 - Present",
+      date: "2022 - Present",
       description: ["Worked primarily in Java 17 EE and Spring Boot microservices development.","Developed real-time applications using Spring JMS, Spring JPA, Spring AOP, and Spring Web.","Built Spring Batch applications for large-scale data processing.","Queried complex data from Oracle DB using SQL and Hibernate.","Implemented RESTful APIs for seamless integration with frontend applications.","Utilized Apigee Proxy for API management and security.","Delivered defect-free products through unit (JUnit, Mockito), integration, and E2E testing.","Worked with AWS services and Linux servers, using Jenkins and Bitbucket for deployment.","Contributed to a US-based BFSI (Banking, Financial Services, and Insurance) client."],
       color: "bg-white-600",
     },
     {
       title: "Bachelor of Engineering",
       subtitle: "Sairam Engineering College",
-      date: "2017 - 2021",
+      date: "2018 - 2022",
       description: ["Learnt programming languages : Java, Python and C.","Gained DSA Knowlege.","Gained Knowlege on CS fundamentals."],
       color: "bg-white-600",
     },
