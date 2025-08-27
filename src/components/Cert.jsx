85d83b98144910c75cde55478f49a6dd81c3dc40
Arun <arunkumard0606@gmail.com>
2025-08-27 20:54:31 +0530

    added margins for mobile devices

--- src/components/Cert.jsx
+++ src/components/Cert.jsx
@@ -6,7 +6,7 @@ const Cert = () => {
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-6 text-center">Certifications</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16 md:m-[50px] m-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16 md:m-[50px] m-5">
         {CERTIFICATIONS.map((cert, index) => (
           <CertificateCard key={index} image={cert.image} title={cert.title} skills={cert.skills} link={cert.link} />
         ))}
--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -16,7 +16,7 @@ function MainBody() {
   };
 
   return (
-    <div className="max-w-[1200px] w-full mx-auto flex flex-col md:flex-row items-center justify-between h-[80vh] md:h-[90vh] px-4 mb-9 sm:mb-0">
+    <div className="max-w-[1200px] w-full mx-auto flex flex-col md:flex-row items-center justify-between h-[80vh] md:h-[90vh] px-4 mb-9 sm:mb-0 mt-5 md:mt-0">
       {/* LEFT SIDE (Text Content) */}
       <div className="flex-1 flex flex-col mt-[-34px]">
         <p className="text-black font-bold p-2 text-center mt-4 md:mt-0">
